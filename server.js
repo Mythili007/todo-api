559cfe15558f88883361ede3fa6638c1f6af27de
Indira Mythili <indira.mythili@gmail.com>
2017-01-12 18:00:54 +0530

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -45,11 +46,11 @@ app.get('/todos/:id', function (req, res) {
 	// 	}
 	// });
 
-	if(matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+if(matchedTodo) {
+	res.json(matchedTodo);
+} else {
+	res.status(404).send();
+}
 
 	// for(var i=0;i<todos.length;i++){
 	// 	if(todoId === todos.id) {
@@ -70,18 +71,24 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if(!_.isBoolean(body.completed) || !_.isString(body.description) 
-		|| body.description.trim().length === 0) {
-		return res.status(400).send();
-	} 
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
-	body.description = body.description.trim();
+// 	if(!_.isBoolean(body.completed) || !_.isString(body.description) 
+// 		|| body.description.trim().length === 0) {
+// 		return res.status(400).send();
+// } 
 
-	body.id = todoNextId;
-	todoNextId++;
-	todos.push(body);
-	// console.log('description: ' + body.description);
-	res.json(body);
+// body.description = body.description.trim();
+
+// body.id = todoNextId;
+// todoNextId++;
+// todos.push(body);
+// 	// console.log('description: ' + body.description);
+// 	res.json(body);
 });
 
 app.delete('/todos/:id', function (req, res) {
@@ -117,16 +124,18 @@ app.put('/todos/:id', function (req, res) {
 	if(body.hasOwnProperty('description') && _.isString(body.description) 
 		&& body.description.trim().length > 0) {
 		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+} else if (body.hasOwnProperty('description')) {
+	return res.status(400).send();
+}
 
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+_.extend(matchedTodo, validAttributes);
+res.json(matchedTodo);
 
 });
 
-
-app.listen(PORT, function () {
-	console.log('Express listening on PORT ' + PORT + '!');
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+		console.log('Express listening on PORT ' + PORT + '!');
+	});
 });
+
